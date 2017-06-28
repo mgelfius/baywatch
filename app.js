fd2a9845a310670e640a9c3e1ac619491ee8612f
Mark Gelfius <mgelfius@purdue.edu>
2017-06-28 17:06:58 -0400

    Add flicks to an unordered list

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const app = {
-    init: function(formSelector){
+    init(selectors){
+        this.flicks = []
         this.max = 0;
+        this.list = document.querySelector(selectors.listSelector)
+        
         document
-            .querySelector(formSelector)
-            .addEventListener('submit', this.handleSubmit.bind(this))
+            .querySelector(selectors.formSelector)
+            .addEventListener(
+                'submit', 
+                this.handleSubmit.bind(this)
+                )
     },
 
-    handleSubmit: function(ev){
+    renderListItem(flick){
+        const item = document.createElement('li')
+        item.textContent = flick.name
+        return item
+    },
+
+    handleSubmit(ev){
         ev.preventDefault()
         const f = ev.target
         const flick = {
             id: this.max + 1,
             name: f.flickName.value,
-
         }
+
+        const listItem = this.renderListItem(flick)
+        this.list.appendChild(listItem)
+
         ++this.max
-        console.log(flick)
     },
 }
 
-app.init('form#flick-form')
+app.init({
+    formSelector: 'form#flick-form',
+    listSelector: '#flick-list',
+})
